4b02866962847bc86db92d285ab45addbebfe9e8
Dev_Yoshi <fujitaniyoshilynp@gmail.com>
2023-08-23 14:32:29 +0800

    Added: dom-purify

--- src/Components.js
+++ src/Components.js
@@ -2,22 +2,26 @@ const Components = [
     {
         id: 1,
         type: "Button",
-        Title: "Button 1"
+        Title: "Button 1",
+        Code: `<button className="bg-yellow-500">Hello World</button>`
     },
     {
         id: 2,
         type: "Button",
-        Title: "Button 2"
+        Title: "Button 2",
+        Code: ""
     },
     {
         id: 3,
         type: "Text",
-        Title: "Text 1"
+        Title: "Text 1",
+        Code: ""
     },
     {
         id: 4,
         type: "Card",
-        Title: "Card 1"
+        Title: "Card 1",
+        Code: ""
     },
 ]
 export default Components
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
 const Card = ({ content }) => {
-    const { Title, type } = content
+    const { Title, type, Code } = content
     return (
-        <div className='bg-gradient-to-b from-[#1C1615] to-[#0A0807] rounded-md min-h-[500px] mx-10'>
-            <h1 className='text-white'>{Title}</h1>
+        <div className='bg-gradient-to-b from-[#1C1615] to-[#0A0807] rounded-md min-h-[500px] mx-10 flex items-center justify-center'>
+            {Code}
         </div>
     )
 }
